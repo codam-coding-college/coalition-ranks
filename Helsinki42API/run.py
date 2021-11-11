c98e2e8237e3c4080c14671054b89b5acad3c88b
limartin <lindsayjamesmartin94@gmail.com>
2021-11-11 15:54:55 +0100

    Made a few changes last time that haven't been committed yet (a few functions have been made and subsequently commented out, so that it doesn't make too many unnecessary API calls when you run it for testing). Also made a list of intra names coupled to user ids. For now I am going to stop working on the project however, as staff has indicated they might want to try a different implementations and I dont want to do unnecessary work.

--- Helsinki42API/run.py
+++ Helsinki42API/run.py
@@ -1,19 +1,68 @@
+# # Max requests is 20 per minute
+
 from intra import ic
 
-
+# Specify Codam as campus_id
 campus_id = 14
-cursus_id = 1
 
-# Filter by campus in specified range of updated_at
+# #Fetch list of all students
+# payload = {
+#     "filter[campus_id]":campus_id
+# }
+# all_students = ic.pages_threaded("campus_users", params=payload)
+
+# for user in all_students:
+#     print(user['id'])
+#     print(user)
+
+# # #Example user
+# # random_user_id = "36085" #Coalition 19
+# # random_user_id = "18510" #Coalition 16
+# # random_user_id = "45658" #Coalition 34
+# # random_user_id = "38105" #Coalition 5
+# # random_id = "34455" 
+# #  GET /v2/users/:user_id/coalitions_users 
+# payload = {
+# }
+# user_coalition_response = ic.get("users/" + random_user_id + "/coalitions_users", payload)
+# data = user_coalition_response.json()
+# for entry in data:
+#     print(entry)
+
+
+# # Get all users in coalition X (X == 19)
+# user_coalition_response = ic.pages_threaded("coalitions/19/coalitions_users", payload)
+# data = user_coalition_response
+# print(data)
+
+# #FETCH A SINGLE USER:
+# user_id = "34455"
+# payload = {
+# 	"filter[id]":user_id
+# }
+# specific_user = ic.pages_threaded("users", params=payload)
+# for entry in specific_user:
+#     print(entry['login'])    
+
+#FETCH CAMPUS USERS:
+ic.progress_bar=True
+
 payload = {
-    "filter[campus_id]":campus_id,
-    # "range[updated_at]":"2019-01-01T00:00:00.000Z,2020-01-01T00:00:00.000Z"
+	"range[login]":"4,zzz"
 }
+specific_user = ic.pages_threaded("campus/14/users", params=payload)
+for entry in specific_user:
+    print(f"{entry['login']} == {entry['id']}") 
 
-# GET campus_users of specified campus in range
-response = ic.get("campus_users", params=payload)
-if response.status_code == 200: # Is the status OK?
-    data = response.json()
 
-for user in data:
-    print(user)
+
+# # user_coalition_response = ic.pages_threaded("coalitions_users", payload)
+
+# # if user_coalition_response.status_code == 200: # Is the status OK?
+# data = user_coalition_response
+
+# print(data)
+# # for entry in data:
+# #     print(entry)
+
+# print(user_coalition_response.status_code)
