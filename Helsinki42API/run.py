9bc7e7e0b6109b6f68b7be049e79f46e352699d4
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-11-15 21:10:02 +0100

    Tidied up run.py into separate functions, rather than a mess of code that gets commented out hahaha. Added a few more utility functions, as well as finding what API numerical values are associated with the Codam coalitions.

--- Helsinki42API/run.py
+++ Helsinki42API/run.py
@@ -5,64 +5,116 @@ from intra import ic
 # Specify Codam as campus_id
 campus_id = 14
 
-# #Fetch list of all students
-# payload = {
-#     "filter[campus_id]":campus_id
-# }
-# all_students = ic.pages_threaded("campus_users", params=payload)
+vela_coalition_id = 60
+cetus_coalition_id = 59
+pyxis_coalition_id = 58
 
-# for user in all_students:
-#     print(user['id'])
-#     print(user)
+# #Example users
+# Anj = 63997 (Cetus)
+# Maarten = 74797 (Pyxis)
+# Peer = 57975 (Vela)
+# Turlough = 64081 (Vela)
+# Lindsay = 64068 (Vela)
 
-# # #Example user
-# # random_user_id = "36085" #Coalition 19
-# # random_user_id = "18510" #Coalition 16
-# # random_user_id = "45658" #Coalition 34
-# # random_user_id = "38105" #Coalition 5
-# # random_id = "34455" 
-# #  GET /v2/users/:user_id/coalitions_users 
-# payload = {
-# }
-# user_coalition_response = ic.get("users/" + random_user_id + "/coalitions_users", payload)
-# data = user_coalition_response.json()
-# for entry in data:
-#     print(entry)
+temp_student_id = 64068
+temp_student_login = "limartin"
 
 
-# # Get all users in coalition X (X == 19)
-# user_coalition_response = ic.pages_threaded("coalitions/19/coalitions_users", payload)
-# data = user_coalition_response
-# print(data)
-
-# #FETCH A SINGLE USER:
-# user_id = "34455"
-# payload = {
-# 	"filter[id]":user_id
-# }
-# specific_user = ic.pages_threaded("users", params=payload)
-# for entry in specific_user:
-#     print(entry['login'])    
-
-#FETCH CAMPUS USERS:
-ic.progress_bar=True
-
-payload = {
-	"range[login]":"4,zzz"
-}
-specific_user = ic.pages_threaded("campus/14/users", params=payload)
-for entry in specific_user:
-    print(f"{entry['login']} == {entry['id']}") 
+def main():
+    print("Program started")
+    # fetch_filtered_students()
+    # fetch_campus_students(campus_id)
+    # who_is_id(temp_student_id)
+    # who_is_login(temp_student_login)
+    # fetch_coalition_info_by_id(temp_student_id)
+    # get_all_users_in_coalition(vela_coalition_id)
+    # fetch_student_info(temp_student_id)
 
 
+# Fetch list of all students
+def fetch_filtered_students():
+    print("Fetching student IDs based on the filters set:")
+    ic.progress_bar = True
+    payload = {
+        "filter[campus_id]": campus_id
+    }
+    all_students = ic.pages_threaded("campus_users", params=payload)
+    for user in all_students:
+        print(user['id'])
 
-# # user_coalition_response = ic.pages_threaded("coalitions_users", payload)
 
-# # if user_coalition_response.status_code == 200: # Is the status OK?
-# data = user_coalition_response
+# FETCH CAMPUS USERS:
+def fetch_campus_students(campus):
+    print("Fetching all students from specified campus:")
+    ic.progress_bar = True
+    payload = {
+        "range[login]": "4,zzz",
+        "sort": "login"
+    }
+    specific_user = ic.pages_threaded("campus/" + str(campus) + "/users", params=payload)
+    for entry in specific_user:
+        print(f"{entry['login']} == {entry['id']}")
 
-# print(data)
-# # for entry in data:
-# #     print(entry)
 
-# print(user_coalition_response.status_code)
+def who_is_id(student_id):
+    print("Finding the intra login associated with that ID:")
+    payload = {
+        "filter[id]": student_id,
+        "range[login]": "4,zzz",
+        "sort": "login"
+    }
+    specific_user = ic.pages_threaded("campus/" + str(campus_id) + "/users", params=payload)
+    for entry in specific_user:
+        print(f"{entry['id']} is {entry['login']}")
+
+
+def who_is_login(login):
+    print("Finding the id for the login specified:")
+    payload = {
+        "filter[login]": login,
+        "range[login]": "4,zzz",
+        "sort": "login"
+    }
+    specific_user = ic.pages_threaded("campus/" + str(campus_id) + "/users", params=payload)
+    for entry in specific_user:
+        print(f"{entry['login']} is {entry['id']}")
+
+
+# GET /v2/users/:user_id/coalitions_users
+def fetch_coalition_info_by_id(student_id):
+    print("Fetching coalition info for specified id:")
+    payload = {
+    }
+    user_coalition_response = ic.get("users/" + str(student_id) + "/coalitions_users", payload)
+    data = user_coalition_response.json()
+    for entry in data:
+        print(entry)
+
+
+# Get all users in coalition
+def get_all_users_in_coalition(coalition_id):
+    print("Fetching all students from specified coalition:")
+    payload = {
+    }
+    user_coalition_response = ic.pages_threaded("coalitions/" + str(coalition_id) + "/coalitions_users", payload)
+    data = user_coalition_response
+    count = 0
+    for entry in data:
+        print(entry)
+        count = count + 1
+    print(f"Total students in this coalition: {count}")
+
+
+# FETCH A SINGLE USER:
+def fetch_student_info(student_id):
+    print("Fetching all general info on student specified by id:")
+    payload = {
+        "filter[id]": student_id
+    }
+    specific_user = ic.pages_threaded("users", params=payload)
+    for entry in specific_user:
+        print(entry)
+
+
+if __name__ == "__main__":
+    main()
